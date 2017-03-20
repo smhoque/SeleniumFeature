fad307c401858f01b5504a06735da6adfb2dc9b7
Ripon <eriponctg@gmail.com>
2017-03-20 02:42:14 -0400

    SeleniumFeature

---
+++ src/TestNGsample/TestNGscripts.java
@@ -0,0 +1,20 @@
+package TestNGsample;
+
+import org.testng.annotations.Test;
+
+public class TestNGscripts {
+  @Test
+  public void loginApplication() {
+	  
+	  System.out.println("This is a login Application");
+  }
+  @Test
+  public void seletItems(){
+	  System.out.println("This is a select Items");
+  }
+  
+  @Test
+  public void CheckOut(){
+	  System.out.println("This is a Checkout");
+  }
+}
